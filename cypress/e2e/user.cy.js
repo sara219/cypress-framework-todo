731b58b96bbd267ab94a7c7d06e4963f8dc227d1
sara219 <sara97abuawwad@gmail.com>
2023-08-27 00:26:59 +0300

    login scenario

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -1,6 +1,11 @@
 /// <reference types="cypress" />
 
 it('Should be able to login', () => {
-    cy.visit('/')
+  cy.visit('/')
+  cy.get('[data-testid="email"]').type('honem39976@trazeco.com')
+  cy.get('[data-testid="password"]').type('Test123!')
 
-});
+  cy.get('[data-testid="submit"]').click()
+
+  cy.get('[data-testid="welcome"]').should('be.visible')
+})
