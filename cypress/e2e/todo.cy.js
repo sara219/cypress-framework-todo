f937b63c81ee76868ae6ad0962688bc396f124e3
sara219 <sara97abuawwad@gmail.com>
2023-08-28 00:27:18 +0300

    login api and add todo api

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,31 +1,51 @@
 /// <reference types="cypress" />
 
-it('Should be able to add a todo', () => {
-  cy.visit('/')
-  cy.get('[data-testid="email"]').type('honem39976@trazeco.com')
-  cy.get('[data-testid="password"]').type('Test123!')
-  cy.get('[data-testid="submit"]').click()
+describe('Todo Test Cases', () => {
+  let accessToken = ''
 
-  cy.get('[data-testid="add"]').click()
-  cy.get('[data-testid="new-todo"]').type('Test')
-  cy.get('[data-testid="submit-newTask"]').click()
-
-  cy.get('[data-testid="todo-item"]').eq(0).should('have.text', 'cypress')
-})
-
-it.only('Should be able to mark as completed', () => {
-  cy.visit('/')
-
-  cy.get('[data-testid="email"]').type('honem39976@trazeco.com')
-  cy.get('[data-testid="password"]').type('Test123!')
-  cy.get('[data-testid="submit"]').click()
-
-  cy.get('[data-testid="add"]').click()
-  cy.get('[data-testid="new-todo"]').type('Test')
-  cy.get('[data-testid="submit-newTask"]').click()
-
-  cy.get('[data-testid="complete-task"]').eq(0).check()
-  cy.get('[data-testid="complete-task"]').should('be.checked')
+  beforeEach(() => {
+    cy.request({
+      url: '/api/v1/users/login',
+      method: 'POST',
+      body: {
+        email: 'honem39976@trazeco.com',
+        password: 'Test123!',
+      },
+    }).then((res) => {
+      expect(res.status).equal(200)
+      accessToken = res.body.access_token
+      //   cy.log(accessToken)
+    })
+    cy.visit('/')
+  })
 
   // ==========
+
+  it('Should be able to add a todo', () => {
+    cy.get('[data-testid="add"]').click()
+    cy.get('[data-testid="new-todo"]').type('Test')
+    cy.get('[data-testid="submit-newTask"]').click()
+  })
+
+  // ==========
+  it.only('Should be able to mark as completed', () => {
+    const newTodo = {
+      item: 'new test for add todo',
+      isCompleted: false,
+    }
+
+    cy.request({
+      url: '/api/v1/tasks',
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: newTodo,
+    }).then((res) => {
+      expect(res.status).equal(201)
+    })
+
+    cy.get('[data-testid="complete-task"]').eq(0).check()
+    cy.get('[data-testid="complete-task"]').should('be.checked')
+  })
 })
