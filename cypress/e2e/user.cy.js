2f6f2039ce68796a38c59e0aefc9631b87ee0b9d
sara219 <sara97abuawwad@gmail.com>
2023-08-28 08:33:35 +0300

    change the structur

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -3,13 +3,16 @@
 import LoginPage from '../pages/login.page'
 
 it('Should be able to login', () => {
+  const validEmail = 'honem39976@trazeco.com'
+  const validPassword = 'Test123!'
+
   // obj from login class
   const loginPage = new LoginPage()
 
   // visit
   loginPage.load()
   // run login
-  loginPage.performLogin('honem39976@trazeco.com', 'Test123!')
+  loginPage.performLogin(validEmail, validPassword)
 
   cy.get('[data-testid="welcome"]').should('be.visible')
 })
