9bc976594eec497fbfb6d07de0f01da312473e2a
sara219 <sara97abuawwad@gmail.com>
2023-09-16 22:30:31 +0300

    todo TC

--- cypress/e2e/todo2resgister.cy.js
+++ cypress/e2e/todo2resgister.cy.js
@@ -1,35 +1,35 @@
-/// <reference types="cypress" />
+// /// <reference types="cypress" />
 
-import TodoApi from '../api/todo.api'
-import UserApi from '../api/user.api'
-import NewTodoPage from '../pages/newTodo.page'
-import TodoPage from '../pages/todo.page'
+// import TodoApi from '../api/todo.api'
+// import UserApi from '../api/user.api'
+// import NewTodoPage from '../pages/newTodo.page'
+// import TodoPage from '../pages/todo.page'
 
-describe('Todo Test Cases', () => {
-  const todoPage = new TodoPage()
+// describe('Todo Test Cases', () => {
+//   const todoPage = new TodoPage()
 
-  let accessToken
-  beforeEach(() => {
-    UserApi.register().then((res) => {
-      accessToken = res.body.access_token
-    })
-  })
+//   let accessToken
+//   beforeEach(() => {
+//     UserApi.register().then((res) => {
+//       accessToken = res.body.access_token
+//     })
+//   })
 
-  // ==========
+//   // ==========
 
-  it.only('Should be able to add a todo', () => {
-    todoPage
-      .load()
-      .clickOnAddBtn()
-      .addNewTodo('Test')
-      .firstItemShouldHaveText('Test')
-  })
+//   it.only('Should be able to add a todo', () => {
+//     todoPage
+//       .load()
+//       .clickOnAddBtn()
+//       .addNewTodo('Test')
+//       .firstItemShouldHaveText('Test')
+//   })
 
-  // ==========
-  it('Should be able to mark as completed', () => {
-    TodoApi.addTodo(accessToken).then((res) => {
-      expect(res.status).equal(201)
-    })
-    todoPage.load().markTodoAsComplete().checkIfCompleted()
-  })
-})
+//   // ==========
+//   it('Should be able to mark as completed', () => {
+//     TodoApi.addTodo(accessToken).then((res) => {
+//       expect(res.status).equal(201)
+//     })
+//     todoPage.load().markTodoAsComplete().checkIfCompleted()
+//   })
+// })
